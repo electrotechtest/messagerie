c523dbafcde3788fc7f4ddd956437884b48fb1b6
Electro_tech_test <78638359+electrotechtest@users.noreply.github.com>
2023-01-05 15:29:04 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,3 +1,4 @@
+mkdir saveData
 echo {"member":[],"message":[],"name":"genesis","creator":0,"tagC":0,"date":"2022-09-24T07:41:14.167Z"} > saveData/conversation.txt
 echo {"tagU":0,"name":"genesis","login":"genesis","password":""} >saveData/users.txt
 echo {"tagC":0,"sender":0,"date":"2022-09-24T07:44:51.254Z","mData":"genesis"} >saveData/message.txt
